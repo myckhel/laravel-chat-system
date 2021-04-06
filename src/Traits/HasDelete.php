0030c6d62aa34fe177ce424b1195c51aeda949ed
myckhel <myckhel1@hotmail.com>
2021-04-06 22:31:23 +0100

    added message meta model

---
+++ src/Traits/HasDelete.php
@@ -0,0 +1,30 @@
+<?php
+namespace Myckhel\ChatSystem\Traits;
+/**
+ *
+ */
+trait HasDelete
+{
+  static function deleteChildren($model, string $relation){
+    if(static::canDeleteRelation($model)){
+      // $model->{$relation}()->chunkById(500, function ($child) {
+      //   $child->fireModelEvent('deleting');
+      //   // Here, we'll touch the owning models, verifying these timestamps get updated
+      //   // for the models. This will allow any caching to get broken on the parents
+      //   // by the timestamp. Then we will go ahead and delete the model instance.
+      //   $child->touchOwners();
+      // });
+
+      $model->{$relation}()->delete();
+    }
+  }
+
+  static function canDeleteRelation($model){
+    if (in_array(SoftDeletes::class, class_uses_recursive($model))) {
+        if (! $model->forceDeleting) {
+            return false;
+        }
+    }
+    return true;
+  }
+}
