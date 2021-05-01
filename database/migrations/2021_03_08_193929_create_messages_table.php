74b318677bfc2847162def5401769d68bcb7211c
myckhel <myckhel1@hotmail.com>
2021-05-01 03:31:53 +0100

    fixed migration typo

--- database/migrations/2021_03_08_193929_create_messages_table.php
+++ database/migrations/2021_03_08_193929_create_messages_table.php
@@ -14,7 +14,7 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $userModel  = config('chat-system.user_model');
+            $userModel  = config('chat-system.models.user');
             $user_table = (new $userModel)->getTable();
             $table->id();
             $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
