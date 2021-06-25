7582f3f56e055ad65ce133364a8575edec3a70e1
Michael Ishola <myckhel123@gmail.com>
2021-06-25 17:44:03 +0100

    only limit conversation query to user if private

--- src/Traits/Message/HasMessage.php
+++ src/Traits/Message/HasMessage.php
@@ -12,34 +12,44 @@ use Myckhel\ChatSystem\Traits\Config;
 trait HasMessage
 {
   use Config;
-  function messages($conversation = null, $otherUser = null, Array $reply = []){
-    if ($conversation || $otherUser) {
-      $conversation = $this->conversations($conversation, $otherUser)->first();
-      // if conversation doesnt exist and add participant
-      if (!$conversation) {
-        $conversation = $this->conversations()->create([
-          'user_id' => $this->id,
-        ]);
-
-        if ($otherUser) {
-          $conversation->participants()->create([
-            'user_id' => $otherUser->id ?? $otherUser,
+  function messages($conversation = null, $otherUser = null, Array $reply = [], $type = 'private'){
+    if ($type == 'private') {
+      if ($conversation || $otherUser) {
+        $conversation = $this->conversations($conversation, $otherUser, type: [$type])->first();
+        // if conversation doesnt exist and add participant
+        if (!$conversation) {
+          $conversation = $this->conversations()->create([
+            'user_id' => $this->id,
           ]);
+
+          if ($otherUser) {
+            $conversation->participants()->create([
+              'user_id' => $otherUser->id ?? $otherUser,
+            ]);
+          }
         }
+        MakeEvent::dispatch($this, 'read', $conversation)->afterResponse();
+        return $conversation->messages()->whereReply($reply);
+      } elseif ($reply) {
+        return self::config('models.message')::whereReplyId($reply['reply_id'])
+        ->whereReplyType($reply['reply_type']);
+      } else {
+        $conversation_ids = $this->conversations(type: [$type])->pluck('conversations.id');
+        return $this->hasMany(self::config('models.message'))
+        ->where(fn ($q) =>
+          $q->orWhereHas('conversation', fn ($q) =>
+            $q->whereIn('conversations.id', $conversation_ids)
+          )
+        )->whereReply($reply)->latest();
       }
-      MakeEvent::dispatch($this, 'read', $conversation)->afterResponse();
-      return $conversation->messages()->whereReply($reply);
-    } elseif ($reply) {
-      return self::config('models.message')::whereReplyId($reply['reply_id'])
-      ->whereReplyType($reply['reply_type']);
     } else {
-      $conversation_ids = $this->conversations()->pluck('conversations.id');
-      return $this->hasMany(self::config('models.message'))
-      ->where(fn ($q) =>
-        $q->orWhereHas('conversation', fn ($q) =>
-          $q->whereIn('conversations.id', $conversation_ids)
-        )
-      )->whereReply($reply)->latest();
+      $conversation = Conversation::whereType($type)
+        ->when($conversation, fn ($q) => $q->whereId($conversation))
+        ->first();
+
+      $conversation && MakeEvent::dispatch($this, 'read', $conversation)->afterResponse();
+
+      return $conversation?->messages();
     }
   }
 
@@ -51,10 +61,11 @@ trait HasMessage
     ->latest();
   }
 
-  function conversations($conversation = null, $otherUser = null){
+  function conversations($conversation = null, $otherUser = null, array $type = []){
     return $this->belongsToMany(
       self::config('models.conversation'), 'conversation_users'
     )->withTimestamps()
+    ->when($type, fn ($q) => $q->whereIn('type', $type))
     ->when($conversation, fn ($q) => $q->where('conversations.id', $conversation->id ?? $conversation))
     ->when($otherUser, fn ($q) => $q->whereHas('participants', fn ($q) => $q->whereUserId($otherUser->id ?? $otherUser)));
   }
