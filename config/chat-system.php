7b723620f887cdde241fb4a7054413b020a4c3f2
Michael Ishola <myckhel123@gmail.com>
2021-07-14 14:40:33 +0100

    added comments to config

--- config/chat-system.php
+++ config/chat-system.php
@@ -2,32 +2,62 @@
 
 return [
   /*
-  * Models
+  * ChatSystem Models
   */
   "models" => [
-    "user"          => "App\\Models\\User",
-    "conversation"  => "Myckhel\\ChatSystem\\Models\\Conversation",
-    "conversation_user"  => "Myckhel\\ChatSystem\\Models\\ConversationUser",
-    "message"       => "Myckhel\\ChatSystem\\Models\\Message",
-    "chat_event"    => "Myckhel\\ChatSystem\\Models\\ChatEvent",
+    /*
+    * The model you want to use as a User model needs to implement the
+    * `Myckhel\ChatSystem\Contracts\ChatEventMaker` contract.
+    */
+    "user"                => "App\\Models\\User",
+
+    /*
+    * The model you want to use as a Conversation model needs to implement the
+    * `Myckhel\ChatSystem\Contracts\IConversation` contract.
+    */
+    "conversation"        => Myckhel\ChatSystem\Models\Conversation::class,
+
+    /*
+    * The model you want to use as a ConversationUser model needs to implement the
+    * `Myckhel\ChatSystem\Contracts\IConversationUser` contract or extends the
+    * `Myckhel\ChatSystem\Models\ConversationUser`
+    */
+    "conversation_user"   => Myckhel\ChatSystem\Models\ConversationUser::class,
+
+    /*
+    * The model you want to use as a Message model needs to implement the
+    * `Myckhel\ChatSystem\Contracts\IMessage` contract or extends the
+    * `Myckhel\ChatSystem\Models\Message`
+    */
+    "message"             => Myckhel\ChatSystem\Models\Message::class,
+
+    /*
+    * The model you want to use as a ChatEvent model needs to implement the
+    * `Myckhel\ChatSystem\Contracts\IChatEvent` contract or extends the
+    * `Myckhel\ChatSystem\Models\ChatEvent`
+    */
+    "chat_event"          => Myckhel\ChatSystem\Models\ChatEvent::class,
   ],
 
   /*
-  * Routes
+  * ChatSystem Routes
+  * Change if you want to add middleware or prefix to
+  * chatSystem routes.
   */
   "route" => [
     "middlewares" => ['api'],
-    "prefix" => 'api'
+    "prefix"      => 'api'
   ],
 
   /*
   * Events Queues
+  * Change if you want to rename the broadcast queue
   */
   "queues" => [
     "events" => [
       "message" => [
         "created" => "chat",
-        "events" => "chat-event",
+        "events"  => "chat-event",
       ],
     ],
     "jobs" => [
@@ -39,11 +69,12 @@ return [
 
   /*
   * Model Observers
+  * The class you want to use for model Observer
   */
   "observers"         => [
     "models"          => [
-      "chat_event"    => 'Myckhel\\ChatSystem\\Observers\\ChatEventObserver',
-      "conversation"  => 'Myckhel\\ChatSystem\\Observers\\ConversationObserver',
+      "chat_event"    => Myckhel\ChatSystem\Observers\ChatEventObserver::class,
+      "conversation"  => Myckhel\ChatSystem\Observers\ConversationObserver::class,
     ]
   ]
 ];
