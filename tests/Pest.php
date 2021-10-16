ad44e703e6041621a3ed4f54392596f9d9765e63
myckhel <myckhel1@hotmail.com>
2021-10-16 18:39:00 +0100

    added more helpers to pest test

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,12 +10,21 @@ uses(TestCase::class)
 
       $this->actingAs(User::inRandomOrder()->first());
 
+      $this->user = auth()->user();
+
       $this->mockUser = fn () => User::create(['name' => $this->faker->name]);
 
       $this->mockConversation = fn ($user, $type = 'private') => $user->conversations()->create([
         'name'    => $this->faker->name.' Group',
         'user_id' => $user->id,
         'type'    => $type,
+      ]);
+
+      $this->conversation = ($this->mockConversation)($this->user);
+
+      $this->mockMessage = fn ($conversation, $user) => $conversation->createMessage([
+        'user_id' => $user->id ?? $user,
+        'message' => $this->faker->sentence
       ]);
     })
     ->in(__DIR__);
