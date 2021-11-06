b6a97d4ad3a1901cf8d3dac82368151676eeb7a2
myckhel <myckhel1@hotmail.com>
2021-11-06 15:01:29 +0100

    changed interface typings

--- src/Contracts/IConversation.php
+++ src/Contracts/IConversation.php
@@ -3,13 +3,12 @@ namespace Myckhel\ChatSystem\Contracts;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use IChatEventMaker;
 
 interface IConversation
 {
   public function participants(): HasMany;
 
-  public function participant(IChatEventMaker $user = null): HasOne;
+  public function participant(IChatEventMaker|int $user = null): HasOne;
 
   public function otherParticipant(IChatEventMaker $user = null): HasOne;
 }
